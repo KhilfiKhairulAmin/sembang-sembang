a6f1f9a8667c4a5849874b226279dfeadabbff84
iNFiENiTE-World <infienite@gmail.com>
2023-06-07 18:14:54 +0800

    Implement social sign in

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import AuthSocialButton from "./AuthSocialButton"
 import { BsGithub, BsGoogle } from "react-icons/bs"
 import { toast } from "react-hot-toast"
+import { signIn } from "next-auth/react"
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -48,14 +49,37 @@ const AuthForm = () => {
     }
 
     if (variant === 'LOGIN') {
-      // NextAuth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false
+      })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error('Invalid credentials')
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success('Logged in!')
+        }
+      })
+      .finally(() => setIsLoading(false))
     }
   }
 
   const socialAction = (action: string) => {
     setIsLoading(true)
 
-    // NextAuth Social Sign In
+    signIn(action, { redirect: false })
+    .then((callback) => {
+      if (callback?.error) {
+        toast.error('Invalid credentials')
+      }
+
+      if (callback?.ok && !callback?.error) {
+        toast.success('Logged in!')
+      }
+    })
+    .finally(() => setIsLoading(false))
   }
 
   return (
