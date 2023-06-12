f4a5c326db8239ab19bda8c89b8bffecad2a1d51
iNFiENiTE-World <infienite@gmail.com>
2023-06-12 21:15:23 +0800

    Update description of metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Sembang-sembang',
-  description: 'Borak bersama',
+  description: 'Borak bersama rakan dan keluarga',
 }
 
 export default function RootLayout({
